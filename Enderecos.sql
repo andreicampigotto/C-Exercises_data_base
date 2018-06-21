15b22893070895c2f991b51f603531e36a9ec220
andreicampigotto <andreicampigotto@gmail.com>
2018-06-21 17:28:07 -0300

    bla
    bla

---
+++ Enderecos.sql
@@ -0,0 +1,19 @@
+﻿/*CREATE TABLE endereco(
+Estado		CHAR(2),
+Cidade		VARCHAR(140),
+Bairro		VARCHAR(120),
+CEP			CHAR(10),
+Logradouro	VARCHAR(250),
+Número		SMALLINT,
+Complemento	TEXT
+);*/
+
+INSERT INTO endereco 
+(Estado,Cidade, Bairro, CEP, Logradouro, Número) 
+VALUES('PB', 'Cabedelo', 'Camalaú', '58.103-052', 'Rua Siqueira Campos', 249);
+
+/*INSERT INTO endereco 
+(Estado, Cidade, Bairro, CEP, Logradouro, Número, Complemento) 
+VALUES('AC',  'Rio Branco', 'Ayrton Senna', '69.911-866', 'Estrada Deputado José Rui da Silveira Lino', 282, 'Casa');*/
+
+SELECT * FROM endereco;
--- PESSOAS.sql
+++ PESSOAS.sql
@@ -6,12 +6,12 @@ Data_Nascimento		DATE,
 Idade				TINYINT
 );*/
 
-INSERT INTO pessoas 
+/*INSERT INTO pessoas 
 (Nome, CPF, RG, Data_Nascimento, Idade) 
-VALUES('Bryan Caio Lopes', '213.217.201-30', '12.949.454-9', '1970-06-18', 48);
+VALUES('Bryan Caio Lopes', '213.217.201-30', '12.949.454-9', '1970-06-18', 48);*/
 
 /*INSERT INTO pessoas 
 (Nome, CPF, RG, Data_Nascimento, Idade) 
 VALUES('Erick Bryan Enrico Vieira', '699.734.958-70', '90.745.276-0', '1989-05-28',29);*/
 
-SELECT * FROM pessoas;
+--SELECT * FROM pessoas;
