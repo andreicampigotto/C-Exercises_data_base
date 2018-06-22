94fdcb7b3b95de4e7345f7f4317836ae8bc1d7da
andreicampigotto <andreicampigotto@gmail.com>
2018-06-22 16:03:15 -0300

    bla
    bla

---
+++ com default.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE quedas (
+	nome		VARCHAR (100) DEFAULT 'NEYMAR',
+	quedas		SMALLINT
+);
+
+INSERT INTO jogos
+	(quedas, nome)
+	VALUES(2, 'Tite');
+
+INSERT INTO jogos
+	(quedas)
+	VALUES (20);
+
+SELECT * FROM jogos;
+
