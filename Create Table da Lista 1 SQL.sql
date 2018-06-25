6ba9875f92748266cecc969cf7691100f1e6f6c7
andreicampigotto <andreicampigotto@gmail.com>
2018-06-25 15:56:31 -0300

    Bla
    Bla

---
+++ Create Table da Lista 1 SQL.sql
@@ -0,0 +1,18 @@
+﻿--DROP TABLE pokemons;
+CREATE TABLE pokemons (
+id						INT IDENTITY(1,1) PRIMARY KEY,
+nome					VARCHAR(255),
+codigo                  VARCHAR(4),
+categoria				VARCHAR(255),
+altura					FLOAT,
+peso					FLOAT,
+hp						INT,
+ataque					INT,
+defesa					INT,
+especial_ataque			INT,
+especial_defesa			INT,
+velocidade				INT,
+descricao				TEXT
+ );
+
+
